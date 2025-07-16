7afc574a933ef16c5256d14b206e89e1e5da7d78
root <mikihsa@gmail.com>
2025-07-16 14:04:58 +0000

    commit from somewhere, yes SOMEwhere...

---
+++ vllm_devc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This script is used to set up the vLLM development environment.
+set -e
+cd scripts
+git clone https://github.com/vllm-project/vllm.git
+echo "[+] vLLM repository cloned successfully. Apply devpatch..."
+chmod +x ./0_container.sh
+./0_container.sh
+echo "[+] vLLM development environment is set up. Pulling latest changes..."
+pip uninstall vllm -y
+cd /vllm-dev
+python setup.py develop

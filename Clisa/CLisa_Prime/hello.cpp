68cbfab58b933ff49ebdf02b8df0d2d4805ded5f
Mikiish <mikihsa@gmail.com>
2025-07-19 00:29:47 +0200

    clean!

--- Clisa/CLisa_Prime/hello.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello from the Temple of Skill!" << std::endl;
-    return 0;
-}
-// This program prints a greeting message to the console.
-// It is written in C++ and uses the C++17 standard.
---
+++ Clisaa/README.md
@@ -0,0 +1,7 @@
+### Visit [https://github.com/Mikiish/Clisa] for more information.
+
+```bash
+cd Clisaa
+git clone https://github.com/Mikiish/Clisa
+```
+

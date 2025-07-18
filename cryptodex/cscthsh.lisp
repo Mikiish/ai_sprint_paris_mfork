a6f8717804c77666ba9ea9e133eda37f8c886ca4
Mikiish <mikihsa@gmail.com>
2025-07-18 22:32:54 +0200

    syncing forking

--- cryptodex/cscthsh.lisp
+++ cryptodex/cscthsh.lisp
@@ -1,32 +1,28 @@
-(load (merge-pathnames "quicklisp/setup.lisp"(user-homedir-pathname)))
+;;;; secret_hash
+;;;; cscthsh.lisp
 
-(ql:quickload :ironclad)
-(ql:quickload :uiop)
+(load (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname)))
+(with-output-to-string (*standard-output*)
+  (ql:quickload '(:ironclad :babel :uiop :alexandria :cl-ppcre)))
 
 (in-package #:cl-user)
 
 ;; ------------------------------------------------------------
-(defun getenv-or-fail (name)
-  "Récupère la variable d’environnement NAME ou quitte avec erreur."
-  (or (uiop:getenv name)
-      (progn
-        (format *error-output* "[ERROR] Environment variable ~a is not set.~%" name)
-        (uiop:quit 1))))
-
-;; ------------------------------------------------------------
 (defun bytes->hex (seq)
-  "Convert a (vector (unsigned-byte 8)) to lowercase hex string."
   (with-output-to-string (s)
     (loop for b across seq do (format s "~2,'0x" b))))
 
 (defun sha256-hex (string)
-  "Return SHA‑256 hex of STRING (UTF‑8)."
   (bytes->hex (ironclad:digest-sequence :sha256 (babel:string-to-octets string :encoding :utf-8))))
 
+(defun final-hash (user-input)
+  (let* ((concat (concatenate 'string (uiop:getenv "TOKEN_HASH") user-input)))
+    (sha256-hex concat)))
+
 (defun main (&optional (user-input ""))
-  "Main function to compute the final hash."
-  (let ((result (sha256-hex user-input)))
-    (format t "~a~%" result))) ; Silent exit when run as script.
-#+sbcl (main (uiop:getenv "API_KEY"))
+  (let ((result (final-hash user-input)))
+    (format t "~a~%" result)))
+;; Silent exit when run as script.
+#+sbcl (main (uiop:getenv "USER_INPUT"))
 ;; ------------------------------------------------------------
 ;; End of script.
--- cryptodex/texst.lisp
+++ cryptodex/texst.lisp
@@ -1,5 +1,5 @@
 ;;;; secret_hash
-;;;; scthsh.lisp
+;;;; texst.lisp
 
 (load (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname)))
 (with-output-to-string (*standard-output*)
@@ -7,7 +7,6 @@
 
 (in-package #:cl-user)
 
-;; ------------------------------------------------------------
 (defun bytes->hex (seq)
   (with-output-to-string (s)
     (loop for b across seq do (format s "~2,'0x" b))))
@@ -15,15 +14,16 @@
 (defun sha256-hex (string)
   (bytes->hex (ironclad:digest-sequence :sha256 (babel:string-to-octets string :encoding :utf-8))))
 
-(defun final-hash (user-input)
-  (let* ((token-hash (sha256-hex (uiop:getenv "AMDDOC_TOKEN")))
-         (concat (concatenate 'string token-hash user-input)))
-    (sha256-hex concat)))
-
 (defun main (&optional (user-input ""))
   (let ((result (final-hash user-input)))
     (format t "~a~%" result)))
 
-#+sbcl (main (uiop:getenv "USER_INPUT"))
+#+sbcl (format t "~a~%" (sha256-hex (uiop:getenv "AMDDOC_TOKEN")))
+;; Make sure you are exporting the variables.
+;; (uiop:setenv "AMDDOC_TOKEN" "your_token_here") to set a variable
+;; (uiop:getenv "AMDDOC_TOKEN") to get a variable.
+;; Also make sure you are using unique name for your variable. Remember that
+;; this is a global variable and it can be overwritten by other scripts.
+;; I suggest especially avoiding things like CODEX_TOKEN or similar, as they might already be in use.
 ;; ------------------------------------------------------------
 ;; End of script.

92687e0a41371e837f9f7cb5066840fee4f3bd3b
Mikiish <mikihsa@gmail.com>
2025-07-18 23:34:37 +0200

    moving this

--- cryptodex/cryptodex-init.lisp
+++
@@ -1,9 +0,0 @@
-;; Charge Quicklisp (pré-installé)
-(load (merge-pathnames "quicklisp/setup.lisp"
-                       (user-homedir-pathname)))
-
-;; Dépendances Cryptodex
-(ql:quickload '(:ironclad :babel :uiop :alexandria :cl-ppcre))
-
-;; Ton programme principal (décommente / ajuste)
-;; (load "secret_hash.lisp")
